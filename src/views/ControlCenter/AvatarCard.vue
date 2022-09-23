f923cca5f4b25ef4c1558c523e3f78f6f67fad3c
BSJJC <805086021@qq.com>
2022-09-23 11:32:43 +0800

    s

--- src/views/ControlCenter/AvatarCard.vue
+++ src/views/ControlCenter/AvatarCard.vue
@@ -1,13 +1,57 @@
 <template>
-  <div class="avatar-card">avatar card</div>
+  <div class="avatar-card">
+    <div class="avatar-img">
+      <el-image :src="avatarUrl">
+        <template #placeholder>
+          <div class="image-slot">Loading<span class="dot">...</span></div>
+        </template>
+      </el-image>
+    </div>
+    <div class="configs">configs</div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const avatarUrl = "https://api.yimian.xyz/img?type=head";
+</script>
 
 <style lang="less" scoped>
+* {
+  transition: all 0.3s ease-in-out;
+}
+
 .avatar-card {
   width: 50%;
   height: 100%;
+  flex-direction: column;
   background-color: lightpink;
+
+  .avatar-img {
+    width: 200px;
+    height: 200px;
+
+    > * {
+      width: 100%;
+      height: 100%;
+      border-radius: 10%;
+    }
+  }
+
+  .configs {
+    width: 100%;
+    height: 0px;
+    background: lightslategrey;
+  }
+
+  &:hover {
+    .avatar-img {
+      width: 150px;
+      height: 150px;
+
+      > * {
+        border-radius: 50%;
+      }
+    }
+  }
 }
 </style>
