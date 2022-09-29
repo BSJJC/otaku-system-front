47a46c8e61a8d02e9e22cd98231af7fd2dca9178
BSJJC <805086021@qq.com>
2022-09-29 13:02:11 +0800

    dsa

--- src/views/ControlCenter/DetailInfo.vue
+++ src/views/ControlCenter/DetailInfo.vue
@@ -12,10 +12,13 @@ const store = useStore();
 
 const back = () => {
   store.state.controlCenterModule.controlCenterClasses.splice(1, 1);
+  store.state.controlCenterModule.controlCenterClasses.push("detail-info-out");
   store.state.appModule.mainRouterViewClasses.push("main-router-view-in");
 
   setTimeout(() => {
     store.state.appModule.mainRouterViewClasses.splice(1, 2);
+    store.state.controlCenterModule.controlCenterClasses.splice(1, 1);
+
     router.back();
   }, 500);
 };
