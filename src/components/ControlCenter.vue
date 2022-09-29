829bc9c476153c3909bcb881e907f283aa9c1d4c
BSJJC <805086021@qq.com>
2022-09-29 14:37:38 +0800

    dsa

--- src/components/ControlCenter.vue
+++ src/components/ControlCenter.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="store.state.appModule.mainRouterViewClasses">
-    <InfoOverview @click="change"></InfoOverview>
+    <InfoOverview></InfoOverview>
     <TeamOverview></TeamOverview>
 
     <router-view :class="store.state.controlCenterModule.controlCenterClasses">
@@ -15,25 +15,7 @@ import InfoOverview from "@/views/ControlCenter/InfoOverview.vue";
 import TeamOverview from "@/views/ControlCenter/TeamOverview.vue";
 import { useStore } from "vuex";
 
-import router from "@/router";
 const store = useStore();
-
-const change = () => {
-  router.push("/ControlCenter/DetailInfo");
-  // includes
-  if (
-    store.state.appModule.mainRouterViewClasses.includes(
-      "main-router-view-hide"
-    )
-  ) {
-    return;
-  }
-  store.state.appModule.mainRouterViewClasses.push("main-router-view-hide");
-  store.state.controlCenterModule.controlCenterClasses.push("detail-info-in");
-  setTimeout(() => {
-    store.state.controlCenterModule.controlCenterClasses.splice(1, 1);
-  }, 500);
-};
 </script>
 
 <style lang="less" scoped>
--- src/store/controlCenterModules/mutations.js
+++ src/store/controlCenterModules/mutations.js
@@ -1,5 +1,25 @@
+import router from "@/router";
+import { useStore } from "vuex";
+
 const mutations = {
-  
+ change() {
+
+  const store = useStore();
+
+  router.push("/ControlCenter/DetailInfo");
+  if (
+   store.state.appModule.mainRouterViewClasses.includes(
+    "main-router-view-hide"
+   )
+  ) {
+   return;
+  }
+  store.state.appModule.mainRouterViewClasses.push("main-router-view-hide");
+  store.state.controlCenterModule.controlCenterClasses.push("detail-info-in");
+  setTimeout(() => {
+   store.state.controlCenterModule.controlCenterClasses.splice(1, 1);
+  }, 500);
+ }
 }
 
 export default mutations
