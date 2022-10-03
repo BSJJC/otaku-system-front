6f1ef5e3b7089ced916eab5f1332cdefaa7177f6
BSJJC <805086021@qq.com>
2022-10-03 20:49:34 +0800

    dsa

--- src/components/ControlCenter.vue
+++ src/components/ControlCenter.vue
@@ -1,13 +1,13 @@
 <template>
   <div :class="store.state.appModule.mainRouterViewClasses">
+    <el-button @click="store.commit(`controlCenterModule/change`)">
+      test
+    </el-button>
     <transition name="control-center-container">
       <div
         class="control-center"
         v-show="store.state.controlCenterModule.controlCenterShow"
       >
-        <el-button @click="store.commit(`controlCenterModule/change`)"
-          >button</el-button
-        >
         <InfoOverview></InfoOverview>
         <TeamOverview></TeamOverview>
       </div>
@@ -22,47 +22,42 @@
 import BackgroundImg from "./BackgroundImg.vue";
 import InfoOverview from "@/views/ControlCenter/InfoOverview.vue";
 import TeamOverview from "@/views/ControlCenter/TeamOverview.vue";
-import { useStore } from "vuex";
 
+import { useStore } from "vuex";
 const store = useStore();
 </script>
 
 <style lang="less" scoped>
-.main-router-view {
+.control-center {
+  width: 90%;
+  height: 100%;
+}
+
+.control-center-container-enter-to,
+.control-center-container-leave-from {
+  transition: all 0.5s ease-in-out;
+  opacity: 1;
+}
+
+.control-center-container-enter-from,
+.control-center-container-leave-to {
+  transition: all 0.5s ease-in-out;
+  opacity: 0;
+}
+
+.info-overview {
+  width: 90vw;
+  height: 60vh;
+}
+
+.team-overview {
+  width: 90vw;
+  height: 40vh;
+}
+
+.detail-info {
   display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-
-  .control-center {
-    width: 90%;
-    height: 100%;
-    background: rgba(255, 0, 0, 0.362);
-  }
-
-  .control-center-container-enter-to,
-  .control-center-container-leave-from {
-    background: lightgreen;
-  }
-
-  .control-center-container-enter-from,
-  .control-center-container-leave-to {
-    background: lightgreen;
-  }
-
-  .info-overview {
-    width: 90vw;
-    height: 60vh;
-  }
-
-  .team-overview {
-    width: 90vw;
-    height: 40vh;
-  }
-
-  .detail-info {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- src/components/UserWelcome.vue
+++ src/components/UserWelcome.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="main-router-view">
+    <button @click="show = !show">Toggle</button>
+    <Transition>
+      <p v-if="show">hello</p>
+    </Transition>
     <h1>welcome welcome</h1>
 
     <router-link
@@ -14,9 +18,22 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import BackgroundImg from "./BackgroundImg.vue";
 import { useStore } from "vuex";
 const store = useStore();
+
+let show = ref(true);
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
