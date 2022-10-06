3150fddfdf94c31f3f703411ae04bf137f5c2ffe
BSJJC <805086021@qq.com>
2022-10-06 13:19:48 +0800

    sA

--- src/views/ControlCenter/CalendarCard.vue
+++ src/views/ControlCenter/CalendarCard.vue
@@ -7,83 +7,110 @@
       <el-button>年</el-button>
     </div>
     <div class="calendar-table">
-      <transition-group name="nums"> 
-
+      <transition-group name="num">
+        <div class="week" v-if="judge == 'week'">
+          <div
+            class="date-card"
+            v-for="(i, index) in year_2022.Jan.weeks[0]"
+            :key="index"
+          >
+            {{ i.title }}
+          </div>
+        </div>
+        <div class="month" v-else-if="judge == 'month'">
+          <div class="date-card" v-for="(i, index) in year_2022" :key="index">
+            {{ index }}
+          </div>
+        </div>
+        <div class="year" v-else-if="judge == 'year'">
+          <div class="date-card">
+            {{ year_2022.year }}
+          </div>
+        </div>
       </transition-group>
     </div>
+
+    <el-button @click="change">change</el-button>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+let judge = ref("week");
 
+let index = 1;
+const change = () => {
+  let arr = ["week", "month", "year"];
+  judge.value = arr[index++];
+  if (index === arr.length) index = 0;
+};
 
 let year_2022 = {
-  arrangements: true,
+  year: 2022,
+  [Symbol("arrangements")]: true,
 
   Jan: {
-    arrangements: true,
+    [Symbol("arrangements")]: true,
 
     weeks: [
       [
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          // 本周第一天
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
       ],
       [
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
         {
-          // 本周第一天
-          arrangements: true,
+          [Symbol("arrangements")]: true,
           title: "there are something you gonna do",
         },
       ],
@@ -98,10 +125,24 @@ let year_2022 = {
   },
 };
 
-console.log(year_2022);
+// console.log(year_2022[Object.getOwnPropertySymbols(year_2022)[0]]);
 </script>
 
 <style lang="less" scoped>
+.num-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.num-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.num-enter-from,
+.num-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
 .calendar-card {
   background: lightblue;
 
@@ -123,6 +164,18 @@ console.log(year_2022);
     height: calc(80% - 20px);
     padding: 10px;
     background: lightgreen;
+    overflow: hidden;
+
+    .week,
+    .month,
+    .year {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      width: 100%;
+      height: 100%;
+    }
 
     .date-card {
       display: flex;
