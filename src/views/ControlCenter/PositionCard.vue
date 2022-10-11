3478259acd386f95226e398331d74ca4ce7ff3fd
BSJJC <805086021@qq.com>
2022-10-11 12:20:50 +0800

    dsa

--- src/views/ControlCenter/PositionCard.vue
+++ src/views/ControlCenter/PositionCard.vue
@@ -1,24 +1,52 @@
 <template>
   <div class="position-card" @click="show">
     <slot name="positionName"></slot>
+
+    <div class="progress">{{}}</div>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, reactive, toRaw } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
-const show = () => {
-  store.commit(`controlCenterModule/change`);
-  sessionStorage.setItem("selectedPosition", JSON.stringify(props.position));
-};
 
 const props = defineProps({
   position: {
     type: String,
   },
 });
+
+const show = () => {
+  store.commit(`controlCenterModule/change`);
+  sessionStorage.setItem("selectedPosition", JSON.stringify(data.position));
+};
+
+const temp = toRaw(props);
+const data = reactive(temp);
+
+const infos = JSON.parse(sessionStorage.getItem("teammemberInfo"));
+
+const positionInfo = infos[data.position.trim()];
+
+const getProgress = (finished, all, obj) => {
+  let finishedTodos = finished ? finished : 0;
+  let allTodos = all ? all : 0;
+
+  if (obj.finished) {
+    finishedTodos++;
+    allTodos++;
+  }
+
+  obj.forEach((todo) => {
+    console.log(todo);
+  });
+
+  return finishedTodos / allTodos;
+};
+
+getProgress(positionInfo.workProgress);
 </script>
 
 <style lang="less" scoped>
