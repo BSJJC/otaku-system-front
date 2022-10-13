553f9b281f9de86270aa0dfbe12de42f0a68c5eb
BSJJC <805086021@qq.com>
2022-10-13 16:21:46 +0800

    das

--- src/views/ControlCenter/TeamOverview.vue
+++ src/views/ControlCenter/TeamOverview.vue
@@ -2,7 +2,10 @@
   <div class="team-overview">
     <PositionCard v-for="(i, index) in positions" :key="index" :position="i">
       <template #positionName>
-        <h2>{{ i }}</h2>
+        <h2 v-if="$t('message.lang') == 'en'">{{ i }}</h2>
+        <h2 v-else-if="$t('message.lang') == 'zh'">
+          {{ positionsZH[i.trim()] }}
+        </h2>
       </template>
     </PositionCard>
   </div>
@@ -10,10 +13,21 @@
 
 <script setup>
 import { reactive } from "vue";
-
 import getItem from "../../api/getItem";
-
 import PositionCard from "./PositionCard.vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+console.log(t);
+
+const positionsZH = {
+  UI: "用户界面",
+  Frontend: "前端",
+  Backend: "后端",
+  Mobile: "手机端",
+  QA: "质量保证",
+  OM: "运行维护",
+};
 
 let positions = reactive([]);
 
