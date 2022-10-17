f1d167e880ca3e563b084fb1e7da38254f7784e0
BSJJC <805086021@qq.com>
2022-10-17 10:16:07 +0800

    das

--- src/components/ControlCenter.vue
+++ src/components/ControlCenter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-router-view">
+  <div class="main-router-view" :class="animates">
     <ConfigCenter></ConfigCenter>
     <transition name="control-center-container">
       <div
@@ -28,11 +28,14 @@
 import BackgroundImg from "./BackgroundImg.vue";
 import InfoOverview from "@/views/ControlCenter/InfoOverview.vue";
 import TeamOverview from "@/views/ControlCenter/TeamOverview.vue";
-
+import { reactive } from "vue";
 import { useStore } from "vuex";
 import ConfigCenter from "./ConfigCenter.vue";
 
 const store = useStore();
+const animates = reactive({
+  "slide-in": true,
+});
 </script>
 
 <style lang="less" scoped>
@@ -69,5 +72,18 @@ const store = useStore();
 .team-overview {
   width: 90vw;
   height: 40vh;
+}
+
+.slide-in {
+  animation: slide-in 0.5s ease-in-out forwards;
+}
+
+@keyframes slide-in {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
 }
 </style>
